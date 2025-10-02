eefc34f69a62b7bed8312883cee5f72a3327e2a4
Nirav Joshi <niravjoshi3000@gmail.com>
2025-10-02 21:16:33 +0530

    added tsup to work with vercel

---
+++ apps/actions/tsup.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["src/index.ts"],
+  format: ["cjs"],
+  target: "node18",
+  bundle: true,
+  clean: true,
+  minify: false,
+  sourcemap: true,
+});
